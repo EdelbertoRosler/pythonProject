9b7680923644bb8ed566f1c5b71ecabfc644b63c
Edelberto Hermann Rosler <82628973+EdelbertoRosler@users.noreply.github.com>
2021-05-19 23:56:27 -0300

    Update exe08.py

--- exe08.py
+++ exe08.py
@@ -5,10 +5,13 @@ exemplo: 5! = 5 * 4 * 3 * 2 * 1 = 120
 
 def fatorial():
     fator = numero = n = int(input("Digite um número: "))
-    for i in range(1, n+1):
-        if n >= 2:
-            fator = fator * (n-1)
-            n -= 1
+    if n == 0 or n == 1:
+        fator = 1
+    else:
+        for i in range(1, n+1):
+            if n >= 2:
+                fator = fator * (n-1)
+                n -= 1
     print('O fatorial de {} é: {}'.format(numero, fator))
 
 if __name__ == '__main__':
